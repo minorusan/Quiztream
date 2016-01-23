1f363dac94e92c36087d4d97365988edb6ef6b83
Minorusan <minorusan8@gmail.com>
2016-01-23 16:09:48 +0200

    Updated server

--- QuizTreamServer.js
+++ QuizTreamServer.js
@@ -4,6 +4,8 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var queries = require('./serverQueries.js');
+var http = require('http');
+
 
 var mongoose = require('mongoose');
 var jwt = require("jsonwebtoken");
@@ -22,6 +24,8 @@ app.use(function (req, res, next) {
 
 });
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 //Initialising MongoDB connection
 
@@ -41,10 +45,10 @@ MongoDB.on('close', function () {
     console.log('Connection closed');
 })
 
-app.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", server_port " + port )
-})
-
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
 
 
 
