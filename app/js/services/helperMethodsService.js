d21c9731ce2a987b6d1651ae269ba1e6d7229143
Klim Schukin <minorusan8@gmail.com>
2016-02-01 16:16:08 +0200

    Init

--- app/js/services/helperMethodsService.js
+++ app/js/services/helperMethodsService.js
@@ -79,7 +79,6 @@ function allHelperMethodsProvider() {
         },
 
         initAvatarSelection: function ($scope, $rootScope) {
-
             $("#openfile").change(function () {
                 var fileToLoad = this.files[0];
                 var fileReader = new FileReader();
@@ -97,8 +96,10 @@ function allHelperMethodsProvider() {
                 e.preventDefault();
                 $("#openfile").trigger('click');
             });
-
-            //Initialising modals for quizes
+     
+        }, 
+        initQuizesModals: function($scope){
+             //Initialising modals for quizes
             setTimeout(function () {
             
                 for (var i = 0; i <= $scope.currentUser.quizes.length - 1; i++) {
