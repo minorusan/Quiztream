3dd34d8b9ffc260b29f8f44c57074068ef552504
Klim Schukin <minorusan8@gmail.com>
2016-02-02 15:49:52 +0200

    Autosave?

--- app/js/controllers/teachersController.js
+++ app/js/controllers/teachersController.js
@@ -58,6 +58,13 @@ function TeachersController($scope, $http, $rootScope, helperMethods) {
     };
 
 
+    $rootScope.$on("$locationChangeStart", function(event, next, current) {
+        if($scope.currentUser != $rootScope.currentUser) {
+            console.log('Yeah, changes were made before page leave.');
+            $scope.saveChanges();
+        }
+    });
+
     $http.post("http://quiztream-quiztreambeta.rhcloud.com/teachers/getTeacherGroups", $scope.currentUser).success(function (data, status, headers, config) {
         
         $scope.currentUser.groups = data;
